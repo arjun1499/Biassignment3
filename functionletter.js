3b29c1ff6685118f0a44d9ab4cf0ad58bd2c525f
arjun1499 <79436322+arjun1499@users.noreply.github.com>
2021-03-14 21:35:26 -0500

    Add files via upload

---
+++ functionletter.js
@@ -0,0 +1,12 @@
+
+function Compare(password1, password2){
+  if (password1.localeCompare(password2) === 0)
+    console.log("The passwords are the same")
+  else
+    console.log("The passwords are not the same")
+  }
+  
+let password1 = prompt("What is your first password?")
+let password2 = prompt("What is your second password?")
+let passCompare = Compare(pass1, pass2)
+
